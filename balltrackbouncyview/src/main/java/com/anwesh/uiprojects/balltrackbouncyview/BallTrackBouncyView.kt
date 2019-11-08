9d57e57ba5bfedcef7c5c2c3a1c3825d9db077f3
Anwesh43 <anweshthecool0@gmail.com>
2019-11-08 09:47:28 +0530

    adding constants and libraries

--- balltrackbouncyview/src/main/java/com/anwesh/uiprojects/balltrackbouncyview/BallTrackBouncyView.kt
+++ balltrackbouncyview/src/main/java/com/anwesh/uiprojects/balltrackbouncyview/BallTrackBouncyView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.balltrackbouncyview
 /**
  * Created by anweshmishra on 08/11/19.
  */
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val scGap : Float = 0.01f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 4f
+val foreColor : Int = Color.parseColor("#1565C0")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
