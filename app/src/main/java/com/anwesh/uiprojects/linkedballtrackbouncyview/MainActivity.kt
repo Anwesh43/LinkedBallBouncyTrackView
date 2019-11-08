8a2caef946469cf7bf550084300a06a99d4b41d7
Anwesh43 <anweshthecool0@gmail.com>
2019-11-08 10:45:18 +0530

    adding view in actiuvity

--- app/src/main/java/com/anwesh/uiprojects/linkedballtrackbouncyview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedballtrackbouncyview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedballtrackbouncyview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.balltrackbouncyview.BallTrackBouncyView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BallTrackBouncyView.create(this)
     }
 }
