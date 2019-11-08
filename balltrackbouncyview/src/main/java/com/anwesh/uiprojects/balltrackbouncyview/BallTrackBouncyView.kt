f96a34ec940e14c40868528bd59fa14da37ab15f
Anwesh43 <anweshthecool0@gmail.com>
2019-11-08 10:07:35 +0530

    adding BallTrackBouncyView class

--- balltrackbouncyview/src/main/java/com/anwesh/uiprojects/balltrackbouncyview/BallTrackBouncyView.kt
+++ balltrackbouncyview/src/main/java/com/anwesh/uiprojects/balltrackbouncyview/BallTrackBouncyView.kt
@@ -54,3 +54,21 @@ fun Canvas.drawBTBNode(i : Int, scale : Float, paint : Paint) {
     drawBouncyTrack(size, scale, h, paint)
     restore()
 }
+
+class BallTrackBouncyView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
