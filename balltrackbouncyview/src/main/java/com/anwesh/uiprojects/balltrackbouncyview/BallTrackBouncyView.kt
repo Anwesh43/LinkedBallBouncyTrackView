2f030d0dd159870d91fdb419759f5f3780de5fd0
Anwesh43 <anweshthecool0@gmail.com>
2019-11-08 10:26:58 +0530

    adding Animator class

--- balltrackbouncyview/src/main/java/com/anwesh/uiprojects/balltrackbouncyview/BallTrackBouncyView.kt
+++ balltrackbouncyview/src/main/java/com/anwesh/uiprojects/balltrackbouncyview/BallTrackBouncyView.kt
@@ -119,4 +119,46 @@ class BallTrackBouncyView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BTBNode(var i : Int, val state : State = State()) {
+
+        private var prev : BTBNode? = null
+        private var next : BTBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BTBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBTBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BTBNode {
+            var curr : BTBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
